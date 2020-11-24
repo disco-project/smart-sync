fe179e09c59019c71f1240d5b3c07f5eb9409644
Matthias Seitz <matthias.seitz@tu-berlin.de>
2020-11-24 20:21:46 +0100

    add SimpleStorage example for listStorageKeys

--- client/scripts/sample-script.ts
+++ client/scripts/sample-script.ts
@@ -21,8 +21,6 @@ async function main() {
     console.log("Greeter deployed to:", greeter.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main()
     .then(() => process.exit(0))
     .catch(error => {
