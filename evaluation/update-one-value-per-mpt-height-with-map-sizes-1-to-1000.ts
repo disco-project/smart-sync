4bcb547bdca93ab26277671f9723217954b8ddad
Martin Westerkamp <westerkamp@tu-berlin.de>
2022-07-25 12:23:45 +0200

    fix evaluation

--- evaluation/update-one-value-per-mpt-height-with-map-sizes-1-to-1000.ts
+++ evaluation/update-one-value-per-mpt-height-with-map-sizes-1-to-1000.ts
@@ -34,10 +34,10 @@ describe('update-one-value-per-mpt-height-with-map-sizes-1-to-1000', async () =>
     let currBlockNr: number;
 
     before(async () => {
-        const fh = new FileHandler(TestCLI.defaultTestConfigFile);
+        const fh = new FileHandler(TestCLI.defaultEvaluationConfigFile);
         chainConfigs = fh.getJSON<TxContractInteractionOptions>();
         if (!chainConfigs) {
-            logger.error(`No config available under ${TestCLI.defaultTestConfigFile}`);
+            logger.error(`No config available under ${TestCLI.defaultEvaluationConfigFile}`);
             process.exit(-1);
         }
         srcProvider = new ethers.providers.JsonRpcProvider({ url: chainConfigs.srcChainRpcUrl, timeout: BigNumber.from(chainConfigs.connectionTimeout).toNumber() });
