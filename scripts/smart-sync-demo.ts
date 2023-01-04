02ebcf3ecdd43f9b6fd18d5a6b969096ab6ccac6
Nemitari Ajienka <nemitariajienka@Taris-MacBook-Pro.local>
2023-01-04 12:29:02 +0000

    update demo script

--- scripts/smart-sync-demo.ts
+++ scripts/smart-sync-demo.ts
@@ -6,7 +6,6 @@ const polygonProvider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL
 
 const goerliSigner = new ethers.Wallet(process.env.PRIVATE_KEY, goerliProvider);
 const polygonSigner = new ethers.Wallet(process.env.PRIVATE_KEY, polygonProvider);
-// const { SimpleStorage, SimpleStorage__factory } = require('../src-gen/types');
 
 const {abi, bytecode} = require("../artifacts/contracts/SimpleStorage.sol/SimpleStorage.json");
 
@@ -14,32 +13,43 @@ const SimpleStorageGoerli = new ethers.ContractFactory(abi, bytecode, goerliSign
 const SimpleStoragePolygon = new ethers.ContractFactory(abi, bytecode, polygonSigner);
 
 async function main() {
-    const simpleStorageGoerli = await SimpleStorageGoerli.deploy();
-    await simpleStorageGoerli.deployed();
-    const simpleStorageGoerliAddress = simpleStorageGoerli.address;
-
-    console.log(`Deployed Goerli contract at: ${simpleStorageGoerliAddress}`);
+    // Goerli
+    // const simpleStorageGoerli = await SimpleStorageGoerli.deploy();
+    // await simpleStorageGoerli.deployed();
+    // const simpleStorageGoerliAddress = simpleStorageGoerli.address;
     // console.log(simpleStorageGoerli.deployTransaction);
-
     // console.log(await simpleStorageGoerli.getA())
-
-    const simpleStoragePolygon = await SimpleStoragePolygon.deploy();
-    await simpleStoragePolygon.deployed();
-    const simpleStoragePolygonAddress = simpleStoragePolygon.address;
-    console.log(`Deployed Polygon Mumbai contract at: ${simpleStoragePolygonAddress}`);
-
-    const newValue = 1337;
-    await (await simpleStoragePolygon.setA(newValue)).wait();
-    const location = ethers.utils.hexConcat([
-        ethers.utils.hexZeroPad(polygonSigner.address, 32), 
-        ethers.utils.hexZeroPad('0x02', 32),
-    ]);
-    const storageKey = ethers.BigNumber.from(ethers.utils.keccak256(location));
-    const storedValue = await polygonProvider.getStorageAt(
-        simpleStoragePolygon.address, 
-        storageKey
+    // console.log(`Deployed Goerli contract at: ${simpleStorageGoerliAddress}`);
+    const goerliAddress = '0x7d57C196D527C50d4E975300EB802F2b6f219559';
+    const simpleStorageGoerli = new ethers.Contract(
+        goerliAddress,
+        abi,
+        goerliSigner
     );
-    console.log(ethers.BigNumber.from(storedValue).toNumber());
+    console.log((await simpleStorageGoerli.getA()).toString());
+
+    // Polygon
+    // const simpleStoragePolygon = await SimpleStoragePolygon.deploy();
+    // await simpleStoragePolygon.deployed();
+    // const simpleStoragePolygonAddress = simpleStoragePolygon.address;
+    // console.log(`Deployed Polygon Mumbai contract at: ${simpleStoragePolygonAddress}`);
+    const newValue = 1337;
+    // await (await simpleStoragePolygon.setA(newValue)).wait();
+    const polygonAddress = '0x1350463D86472B4BbBfcD4936807168df93Ea639';
+    const simpleStoragePolygon = new ethers.Contract(
+        polygonAddress,
+        abi, 
+        polygonSigner
+    );
+    console.log((await simpleStoragePolygon.getA()).toString());
+
+    // the first variable stored by the contact is a
+    // so it should be at index 0 in the storage.
+    const itemAtStorage = await polygonProvider.getStorageAt(simpleStoragePolygon.address, 0);
+    console.log(itemAtStorage, await simpleStoragePolygon.getA())
+    console.log(itemAtStorage._hex == await simpleStoragePolygon.getA()); // false because item at storage is padded
+    console.log(ethers.BigNumber.from(itemAtStorage).toNumber() === newValue);
+
 }
 
 main();
