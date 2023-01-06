237ce463ab76feab487bfdd78d802c9a5ea2b056
Rafael Belchior <rbelchior@blockdaemon.com>
2023-01-06 17:15:07 +0000

    chore: update env vars
    Signed-off-by: Rafael Belchior <rbelchior@blockdaemon.com>

--- demo/smart-sync-demo.ts
+++ demo/smart-sync-demo.ts
@@ -5,7 +5,10 @@ require('dotenv').config();
 
 logger.setSettings({ minLevel: 'info', name: 'demo' });
 
-const { PRIVATE_KEY, DEPLOYED_CONTRACT_ADDRESS_GOERLI, DEPLOYED_CONTRACT_ADDRESS_MUMBAI, RPC_URL_GOERLI, RPC_URL_MUMBAI } = process.env;
+const {
+    PRIVATE_KEY, DEPLOYED_CONTRACT_ADDRESS_GOERLI, DEPLOYED_CONTRACT_ADDRESS_MUMBAI, RPC_URL_GOERLI, RPC_URL_MUMBAI, RPC_LOCALHOST_ORIGIN, RPC_LOCALHOST_TARGET,
+} = process.env;
+
 
 const goerliProvider = new ethers.providers.JsonRpcProvider(RPC_URL_GOERLI);
 const polygonProvider = new ethers.providers.JsonRpcProvider(RPC_URL_MUMBAI);
